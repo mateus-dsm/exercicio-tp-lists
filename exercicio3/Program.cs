2fb353aaa37d635b726aa54301647185089453e2
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2023-02-06 15:09:59 -0300

    Add files via upload

---
+++ exercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static string ValorMaisProximoDeZero(List<int> numeros)
+    {
+        if (numeros.Count == 0) return "Lista vazia";
+
+        bool nenhum = false;
+        int maisProximo = numeros[0];
+        int distanciaDoZero = Math.Abs(maisProximo);
+
+        foreach (int numero in numeros)
+        {
+            if(Math.Abs(numero) < distanciaDoZero)
+            {
+                maisProximo = numero;
+                distanciaDoZero = Math.Abs(numero);
+                nenhum = false;
+            } else if(Math.Abs(numero) == distanciaDoZero && numero != maisProximo) nenhum = true;
+        }
+
+        if (nenhum) return "Nenhum";
+
+        return maisProximo.ToString();
+    }
+
+    static void Main(string[] args)
+    {
+        List<int> listaNumeros = new List<int>();
+        int[] numeros = { 13, 0, -6};
+        listaNumeros.AddRange(numeros);
+        Console.WriteLine(ValorMaisProximoDeZero(listaNumeros));
+    }
+}
