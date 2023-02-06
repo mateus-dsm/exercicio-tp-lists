0515fad16fa760f7d4f4ac49876e24d666832a43
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2023-02-06 15:08:34 -0300

    Add files via upload

---
+++ exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static int DiferencaEntreMaiorEMenor(List<int> numeros)
+    {
+        if(numeros.Count == 0 || numeros.Count == 1) return 0;
+
+        int menor = numeros[0];
+        int maior = numeros[0];
+
+        foreach(int numero in numeros)
+        {
+            if(numero < menor) menor = numero;
+            else if(numero > maior) maior = numero;
+        }
+
+        return maior - menor;
+    }
+
+    static void Main(string[] args)
+    {
+        List<int> listaNumeros = new List<int>();
+        Console.WriteLine(DiferencaEntreMaiorEMenor(listaNumeros));
+
+        listaNumeros.Add(1);
+        Console.WriteLine(DiferencaEntreMaiorEMenor(listaNumeros));
+
+        int[] numeros = { 2, -4, 3 };
+        listaNumeros.AddRange(numeros);
+        Console.WriteLine(DiferencaEntreMaiorEMenor(listaNumeros));
+    }
+}
